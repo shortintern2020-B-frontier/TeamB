762c87b11743152be3bb28d75c4d533181d51c42
平沼 智之 <tomoyuki.hiranuma110664@e-seikatsu.co.jp>
2020-09-05 21:06:04 +0900

    パスワードハッシュ化

--- back_end/db/schema.rb
+++ back_end/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200905062624) do
+ActiveRecord::Schema.define(version: 20200904100708) do
 
   create_table "chats", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20200905062624) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
+    t.string "password"
     t.string "profile"
     t.integer "room_id"
     t.datetime "created_at", null: false
