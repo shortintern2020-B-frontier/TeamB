dc2667a1438814256073a92bdd01e21cd0c2420a
rurito0125 <rurito.blog@gmail.com>
2020-09-05 14:04:05 +0900

    rootに登録ページを組み込んだ

--- front_end/app/src/root.js
+++ front_end/app/src/root.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Main } from './components/main/index';
 import Toppage from './components/top/index';
+import Signup from './components/signup/index';
 
 const Root = () => (
   <Router>
     <Switch>
       <Route exact path="/" render={() => <Main />} />
+      <Route exact path="/signup" render={() => <Signup />} />
       <Route exact path="/toppage" render={() => <Toppage />} />
     </Switch>
   </Router>
