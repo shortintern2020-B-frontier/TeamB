8fce5d1d8844dfc2b3fbc40927a4732292dd952a
rurito0125 <rurito.blog@gmail.com>
2020-09-06 03:00:39 +0900

    redux-persisitのセットアップを行った

--- front_end/app/src/index.js
+++ front_end/app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers/rootReducer';
@@ -12,15 +15,27 @@ const App = () => (
   <Root />
 );
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   applyMiddleware(thunk, logger),
 );
+
+const persist_store = persistStore(store)
 
 const app = document.getElementById('app'); // eslint-disable-line no-undef
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persist_store}>
+      <App />
+    </PersistGate>
   </Provider>,
   app,
 );
