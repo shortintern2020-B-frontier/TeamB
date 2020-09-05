78784400ca5993cc70208d223322cb9511af1d92
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2020-09-05 23:22:56 +0900

    frontとbackendのネットワーク設定

--- front_end/app/src/components/main/index.js
+++ front_end/app/src/components/main/index.js
@@ -1,13 +1,24 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getPosts } from '../../actions/postAction';
+import axios from 'axios';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 export const mainSelector = (state) => {
   const { length } = state.posts;
   return state.posts[length - 1];
 };
 
+export const check = () => {
+  axios.get('http://localhost:5000/api/v1/hello#show')
+  .then((res) => console.log(res.data))
+  .catch((err) => console.log(err));
+}
+
 export const PostList = (posts) => {
+  useEffect(() => {
+    check();
+  },[])
   if (posts.isFetching) {
     return (
       <p>loading</p>
