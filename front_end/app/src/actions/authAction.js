897976bb9e6dce45bae8e2416c5150e3d75573e4
rurito0125 <rurito.blog@gmail.com>
2020-09-06 01:22:19 +0900

    ログインActionを実装

--- front_end/app/src/actions/authAction.js
+++ front_end/app/src/actions/authAction.js
@@ -1,1 +1,30 @@
 import axios from '../settings/axios';
+
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const loginRequest = () => ({
+  type: LOGIN_REQUEST,
+});
+
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const loginSuccess = () => ({
+  type: LOGIN_SUCCESS,
+});
+
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+export const loginFailure = () => ({
+  type: LOGIN_FAILURE,
+});
+
+// TODO: 実際のapiを叩く箇所を実装する
+/**
+ * ログインアクションをdispatchする
+ * @param {Object} user - ログインに必要なデータが格納されている
+ * @param {string} user.name - ユーザー名
+ * @param {string} user.password - パスワード
+ */
+export const login = (user) => (dispatch) => {
+  dispatch(loginRequest());
+  return axios.post('http://localhost:8000/users', { user })
+    .then((res) => dispatch(loginSuccess(user)))
+    .catch((err) => dispatch(loginFailure(err)));
+};
