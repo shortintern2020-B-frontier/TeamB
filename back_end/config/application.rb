d63f52448016a7f150ba848fa76c10bb9c305d8e
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2020-09-05 17:10:54 +0900

    [WIP]factorybotの導入を行った。

--- back_end/config/application.rb
+++ back_end/config/application.rb
@@ -29,5 +29,14 @@ module Myapp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    #rikuiwasaki
+    config.generators do |g|
+      g.test_framework :rspec,
+      view_specs:false,
+      helper_specs:false,
+      controller_specs:false,
+      routing_specs:false
+    end
+    #rikuiwasaki
   end
 end
