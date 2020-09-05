d4207486d88940aa26091e22c3f956430e23ab6a
rurito0125 <rurito.blog@gmail.com>
2020-09-06 01:01:33 +0900

    ログイン情報を入力するフォームを作成

--- front_end/app/src/components/auth/signin.js
+++ front_end/app/src/components/auth/signin.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
-const Signin = () => (
-  <div>
-    <p>this is sign in page</p>
-  </div>
-);
+/**
+ * フォームのログインボタンがクリックされた際に発火する
+ * フォームに入力された内容を用いてログイン処理を行う
+ * @param {Object} data - 入力されたデータ
+ * @param {string} data.name - ユーザー名
+ * @param {string} data.password - パスワード
+ */
+const Submit = (data) => {
+  console.log(data);
+}
+
+const Signin = () => {
+  const { register, handleSubmit } = useForm();
+
+  return (
+    <div>
+      <p>this is sign in page</p>
+      <form onSubmit={handleSubmit(Submit)}>
+        <TextField
+          name="name"
+          label="ユーザー名"
+          inputRef={register}
+        />
+        <TextField
+          name="password"
+          label="パスワード"
+          inputRef={register}
+        />
+        <Button type="submit">
+          ログイン
+        </Button>
+      </form>
+    </div>
+  );
+};
 
 export default Signin;
