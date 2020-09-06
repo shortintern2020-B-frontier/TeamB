76ce5a1189fad170fbaf66c741d5dde1360a73ec
rurito0125 <rurito.blog@gmail.com>
2020-09-07 02:16:01 +0900

    signupのactionを実装

--- front_end/app/src/actions/authAction.js
+++ front_end/app/src/actions/authAction.js
@@ -36,6 +36,24 @@ export const loginFailure = (error) => ({
   error,
 });
 
+export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
+export const signupRequest = () => ({
+  type: SIGNUP_REQUEST,
+});
+
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const signupSuccess = (token) => ({
+  type: SIGNUP_SUCCESS,
+  token,
+  receivedAt: Date.now(),
+});
+
+export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+export const signupFailure = (error) => ({
+  type: SIGNUP_FAILURE,
+  error,
+});
+
 // TODO: 実際のapiを叩く箇所を実装する
 // TODO: redux-thunkに置き換える
 /**
@@ -52,6 +70,24 @@ export const login = (user) => (dispatch) => {
       dispatch(loginSuccess(res.data));
     })
     .catch((err) => dispatch(loginFailure(err)));
+};
+
+// TODO: 実際のapiを叩く箇所を実装する
+// TODO: redux-thunkに置き換える
+/**
+ * 登録アクションをdispatchする
+ * @param {Object} user - ログインに必要なデータが格納されている
+ * @param {string} user.name - ユーザー名
+ * @param {string} user.password - パスワード
+ */
+export const signup = (user) => (dispatch) => {
+  dispatch(signupRequest());
+  return axios.post('http://localhost:8000/auth/login', user)
+    .then((res) => {
+      localStorage.setItem('jwt', res.data.access_token);
+      dispatch(signupSuccess(res.data));
+    })
+    .catch((err) => dispatch(signupFailure(err)));
 };
 
 /**
--- front_end/app/src/components/auth/signup.js
+++ front_end/app/src/components/auth/signup.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { signup } from '../../actions/authAction';
 
-const Signup = () => (
-  <div>
-    <p>this is sign up page</p>
-  </div>
-);
+const Signup = () => {
+  const { register, handleSubmit } = useForm();
+  const dispatch = useDispatch();
+
+  /**
+   * フォームの登録ボタンがクリックされた際に発火する
+   * フォームに入力された内容を用いて登録処理を行う
+   * @param {Object} data - 入力されたデータ
+   * @param {string} data.name - ユーザー名
+   * @param {string} data.password - パスワード
+   */
+  const Submit = (data) => {
+    // jsonデータに変形してから投げる
+    dispatch(signup(JSON.stringify(data)));
+  };
+
+  return (
+    <div>
+      <p>this is sign up page</p>
+      <form onSubmit={handleSubmit(Submit)}>
+        <TextField
+          name="name"
+          label="ユーザー名"
+          inputRef={register}
+        />
+        <TextField
+          name="password"
+          label="パスワード"
+          inputRef={register}
+        />
+        <Button type="submit">
+          登録
+        </Button>
+      </form>
+    </div>
+  );
+};
 
 export default Signup;
