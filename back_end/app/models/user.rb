8aa5d26e2a85540b72ed4d8f3a1c0b7c6c63e0c0
平沼 智之 <tomoyuki.hiranuma110664@e-seikatsu.co.jp>
2020-09-06 22:56:08 +0900

    relationships修正

--- back_end/app/models/user.rb
+++ back_end/app/models/user.rb
@@ -13,12 +13,12 @@ class User < ApplicationRecord
 
 	def follow(other_user)
 		unless self == other_user
-			self.Relationships.find_or_create_by(follow_id: other_user.id)
+			self.relationships.find_or_create_by(follow_id: other_user.id)
 		end
 	end
 
 	def unfollow(other_user)
-		relationship = self.Relationships.find_by(follow_id: other_user.id)
+		relationship = self.relationships.find_by(follow_id: other_user.id)
 		if relationship
 			relationship.destroy
 		end
