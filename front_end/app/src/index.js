6adebc62167a7d6795c563972ea1290fef6ac98a
rurito0125 <rurito.blog@gmail.com>
2020-09-06 18:11:03 +0900

    redux-persistを削除

--- front_end/app/src/index.js
+++ front_end/app/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
-import { persistReducer, persistStore } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 
 import rootReducer from './reducers/rootReducer';
@@ -15,27 +12,15 @@ const App = () => (
   <Root />
 );
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  whitelist: ['auth']
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 const store = createStore(
-  persistedReducer,
+  rootReducer,
   applyMiddleware(thunk, logger),
 );
-
-const persist_store = persistStore(store)
 
 const app = document.getElementById('app'); // eslint-disable-line no-undef
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persist_store}>
-      <App />
-    </PersistGate>
+    <App />
   </Provider>,
   app,
 );
