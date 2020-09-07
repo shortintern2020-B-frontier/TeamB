0da201185675b302e9b0fbdf3ddac6442f018e22
横田喬介 <kyosuke281610@gmail,com>
2020-09-07 15:39:26 +0900

    ユーザーのタグ選択設定

--- back_end/app/controllers/api/v1/user_tags_controller.rb
+++ back_end/app/controllers/api/v1/user_tags_controller.rb
@@ -1,6 +1,28 @@
+'''
+  author: Kyosuke Yokota
+  Date: 20200907
+'''
+
 module Api
     module V1
         class UserTagsController < ApplicationController
+            include JwtAuthenticator
+            jwt_authenticate
+            
+            def create
+                user_tag_info = user_tag_params
+                user_tag_info[:user_id] = @current_user.id
+                user_tag = UserTag.new(user_tag_info)
+                if user_tag.save
+                  render json: { status: 'SUCCESS', data: { user: user_tag } }
+                else
+                  render json: { status: 'ERROR', data: { error: user_tag.errors } }
+                end
+            end
+
+            def user_tag_params
+                params.require(:user_tag).permit(:tag_id)
+            end
         end
     end
 end
--- back_end/app/models/tag.rb
+++ back_end/app/models/tag.rb
@@ -5,6 +5,7 @@
 
 class Tag < ApplicationRecord
     # modelの関係性については確認したいので一旦コメント
-    # has_many :user_tag, :room_tag
+    # has_many :user_tag #:room_tag
+    has_and_belongs_to_many :users
     validates :name, presence: true
 end
