9af7f01569a69e125dcf8b8b433b7e213b9241d0
平沼 智之 <tomoyuki.hiranuma110664@e-seikatsu.co.jp>
2020-09-07 09:38:12 +0900

    youtube_idをintからstringへ

--- back_end/db/schema.rb
+++ back_end/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200906101515) do
+ActiveRecord::Schema.define(version: 20200907003646) do
 
   create_table "chats", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id"
@@ -39,13 +39,13 @@ ActiveRecord::Schema.define(version: 20200906101515) do
 
   create_table "rooms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
-    t.integer "youtube_id"
     t.integer "admin_id"
     t.boolean "is_private"
     t.string "password"
     t.timestamp "start_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "youtube_id"
   end
 
   create_table "tags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
