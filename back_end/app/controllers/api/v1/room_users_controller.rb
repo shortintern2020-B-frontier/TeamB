d2184dadf5d8428219a12a3e5595d1e390261781
平沼 智之 <tomoyuki.hiranuma110664@e-seikatsu.co.jp>
2020-09-07 15:43:15 +0900

    current_user.saveのエラーあり

--- back_end/app/controllers/api/v1/room_users_controller.rb
+++ back_end/app/controllers/api/v1/room_users_controller.rb
@@ -1,6 +1,35 @@
+'''
+	Author: Hiranuma Tomoyuki
+	Date: 20200907
+'''
+
 module Api
-    module V1
-        class RoomUsersController < ApplicationController
-        end
-    end
+	module V1
+		class RoomUsersController < ApplicationController
+			jwt_authenticate
+
+			def create
+				if @current_user.update(room_id_params)
+					render json: { status: "SUCCESS", data: { user: @current_user } }
+				else
+					render json: { status: "ERROR", data: { user: @current_user } }
+				end
+			end
+
+			def destroy
+				@current_user.room_id = nil
+				if @current_user.save
+					render json: { status: "SUCCESS", data: { user: @current_user } }
+				else
+					render json: { status: "ERROR", data: { user: @current_user } }
+				end
+			end
+			
+			private
+			def room_id_params
+				params.require(:user).permit(:room_id)
+			end
+
+		end
+	end
 end
