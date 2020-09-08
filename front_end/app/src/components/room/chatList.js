7d4632e9c3d588ec0e75043694bd00c9c861cc5d
rurito0125 <rurito.blog@gmail.com>
2020-09-09 04:41:36 +0900

    scroolbarのプラグインを導入して組み込み

--- front_end/app/src/components/room/chatList.js
+++ front_end/app/src/components/room/chatList.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Scrollbars } from "react-custom-scrollbars";
+import { makeStyles } from "@material-ui/core/styles";
+
+const styles = makeStyles({
+  List: {
+    height: "200px",
+  },
+});
 
 const chatSelector = (state) => state.chat.msgs;
 
 export const ChatList = () => {
   const msgs = useSelector(chatSelector);
+  const classes = styles();
 
   return (
-    <ul>
-      {
-        msgs.map((msg, index) => (
-          <li key={index.toString()}>
-            <p>
-              { index }
-              番目:
-              {' '}
-              { msg }
-            </p>
-          </li>
-        ))
-      }
-    </ul>
+    <div className={classes.List}>
+      <Scrollbars>
+        <ul>
+          {
+            msgs.map((msg, index) => (
+              <li key={index.toString()}>
+                <p>
+                  { index }
+                  番目:
+                  {' '}
+                  { msg }
+                </p>
+              </li>
+            ))
+          }
+        </ul>
+      </Scrollbars>
+    </div>
   );
 };
