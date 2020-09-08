12a0e5ec98089671aa2d6f8588730f30d47b6556
横田喬介 <kyosuke281610@gmail,com>
2020-09-08 18:13:55 +0900

    ユーザーのタグに関連するルームを検索する

---
+++ back_end/app/controllers/api/v1/user_room_tags_controller.rb
@@ -0,0 +1,34 @@
+'''
+  author: Kyosuke Yokota
+  Date: 20200907
+'''
+
+module Api
+    module V1
+        class UserRoomTagsController < ApplicationController
+            include JwtAuthenticator
+            jwt_authenticate
+            before_action :set_tag, only: [:show]
+            def show
+                @room = Room.where(id: @room_ids)
+                render json: { status: 'test', data: { data: @room } }
+            end
+
+            def set_tag 
+                @tag = @current_user.tags
+                tag_array = []
+                @tag.each do |t|
+                    tag_array.push(t.id)                    
+                end
+
+                @room_tag = RoomsTag.where(tag_id: tag_array)
+                @room_ids = []
+                @room_tag.each do |t|
+                    @room_ids.push(t.room_id)
+                end
+
+            end
+        end
+    end
+end
+
