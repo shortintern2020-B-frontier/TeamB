f133c1a427c4de6b652fadd0968ae91bc3092ea5
横田喬介 <kyosuke281610@gmail,com>
2020-09-08 11:28:45 +0900

    tag関連のリレーションを修正

---
+++ back_end/db/migrate/20200908020140_create_rooms_tags.rb
@@ -0,0 +1,10 @@
+class CreateRoomsTags < ActiveRecord::Migration[5.1]
+  def change
+    create_table :rooms_tags, id: false do |t|
+      t.belongs_to :room
+      t.belongs_to :tag
+
+      t.timestamps
+    end
+  end
+end
