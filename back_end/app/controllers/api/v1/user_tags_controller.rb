350cf3e2cb290195aa981d19e9d2dd94fd37a3f2
taiki1015 <karakawa1015@gmail.com>
2020-09-08 11:08:18 +0900

    Userの持つtagsを表示できるように変更

--- back_end/app/controllers/api/v1/user_tags_controller.rb
+++ back_end/app/controllers/api/v1/user_tags_controller.rb
@@ -8,7 +8,9 @@ module Api
         class UserTagsController < ApplicationController
             include JwtAuthenticator
             jwt_authenticate
-            
+
+            before_action :set_user , only: [:show]
+
             def create
                 user_tag_info = user_tag_params
                 user_tag_info[:user_id] = @current_user.id
@@ -19,6 +21,18 @@ module Api
                   render json: { status: 'ERROR', data: { error: user_tag.errors } }
                 end
             end
+
+            #karakawa
+            #userの持つtagを表示
+            def show
+              @tags = @user.tags
+              render json: { status: 'SUCCESS', data: { tags: @tags} }
+            end
+
+            def set_user
+              @user = User.find(parms[:id])
+            end
+            #karakawa  
 
             def user_tag_params
                 params.require(:user_tag).permit(:tag_id)
