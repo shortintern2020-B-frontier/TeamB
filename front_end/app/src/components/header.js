7de2d8f08f5f1db93887559eac369aa6580588d6
rurito0125 <rurito.blog@gmail.com>
2020-09-08 12:46:47 +0900

    lintとtestに従い修正

--- front_end/app/src/components/header.js
+++ front_end/app/src/components/header.js
@@ -6,12 +6,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import CreateRoomDialog from './room/createRoomDialog';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import CreateRoomDialog from './room/createRoomDialog';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -50,9 +50,9 @@ const Header = () => {
 
   const moveTags = () => {
     history.push('/tags');
-  }
+  };
 
-  if( auth.isLoggedIn ) {
+  if (auth.isLoggedIn) {
     return (
       <header className={classes.root}>
         <AppBar position="static" className={classes.appBar}>
@@ -66,22 +66,21 @@ const Header = () => {
           </Toolbar>
         </AppBar>
       </header>
-    )
-  } else {
-    return (
-      <header className={classes.root}>
-        <AppBar position="static" className={classes.appBar}>
-          <Toolbar>
-            <Typography variant="h6" className={classes.title}>
-              TheaTalk
-            </Typography>
-            <Button onClick={moveSignup}>Sign Up</Button>
-            <Button onClick={moveLogin}>Login</Button>
-          </Toolbar>
-        </AppBar>
-      </header>
     );
   }
+  return (
+    <header className={classes.root}>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            TheaTalk
+          </Typography>
+          <Button onClick={moveSignup}>Sign Up</Button>
+          <Button onClick={moveLogin}>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </header>
+  );
 };
 
 export default Header;
