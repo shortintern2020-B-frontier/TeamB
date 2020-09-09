d780c7f78a7a971666e55338fed25f7f126411ea
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2020-09-09 09:46:16 +0900

    chat_controllのstatuscodeを追加

--- back_end/app/controllers/api/v1/chats_controller.rb
+++ back_end/app/controllers/api/v1/chats_controller.rb
@@ -1,35 +1,36 @@
 module Api
     module V1
         class ChatsController < ApplicationController            #rikuiwasaki
-            jwt_authenticate except: :index
+            jwt_authenticate
 
             def index
-                chats = Chat.where(room_id: params[:room_id]).order(updated_at: :desc)
-                render json: { status: 'SUCCESS',data: {
+                if chats = Chat.where(room_id: @current_user.room_id).order(updated_at: :desc)
+                    render json: { status: 'SUCCESS',data: {
                     chats: chats } }
+                else
+                    render status:500, json: { status: 'ERROR', data:{ error: "情報を取得できませんでした。"}}
             end
             def create
                 chat_info=chat_params
                 chat_info[:user_id] = @current_user.id
                 @new_chat=Chat.new(chat_info)
                 if @new_chat.save
-                    @room=Room.find(1)
                     RoomChannel.broadcast_to("room_#{chat_info[:room_id]}",@new_chat)
-                    render json: { status: 'SUCCESS', data: { chat: @new_chat } }
+                    render status:201, json: { status: 'SUCCESS', data: { chat: @new_chat } }
                 else 
-                    render json: { status: 'ERROR', data: { error: errors }}
+                    render status:500, json: { status: 'ERROR', data: { error: "情報を取得できませんでした" }}
                 end
             end
             def update
                 chat = Chat.find(params[:id])
                 if chat.user_id == @current_user.id
                     if updated_chat=Chat.update(chat_params)
-                        render json: { status: 'SUCCESS', data: {chat: updated_chat}}
+                        render status:201 json: { status: 'SUCCESS', data: {chat: updated_chat}}
                     else
-                        render json: { status: 'ERROR', data: { errors: updated_chat.errors}}
+                        render status:500 json: { status: 'ERROR', data: { errors: "情報を取得できませんでした。"}}
                     end
                 else
-                    render json: {status: 'ERROR', data:{erros: "invalid user"}}
+                    render status:401, json: {status: 'ERROR', data:{erros: "invalid user"}}
                 end
             
             end
