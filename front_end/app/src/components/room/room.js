eb4b500926147bcba6aa3f4b56373cb92c3867eb
平沼 智之 <tomoyuki.hiranuma110664@e-seikatsu.co.jp>
2020-09-09 18:59:42 +0900

    間違ったコンフリクトの解消をしたので修正

--- front_end/app/src/components/room/room.js
+++ front_end/app/src/components/room/room.js
@@ -59,10 +59,6 @@ const Room = () => {
   }, []);
 
   useEffect(() => {
-    const local_room = JSON.parse(localStorage.getItem('room'));
-    if (local_room !== null && room.youtube_id !== local_room.youtube_id) {
-      dispatch(setRoom(local_room));
-    }
     setVideo(BASE_URL + room.youtube_id)
   }, [room])
 
